f41fc9727808e9a7c60801c1ac031de88a739a33
konstantina.basta <konstantina.basta@eurodyn.com>
2022-04-19 10:21:15 +0300

    init commit

---
+++ src/main/java/com/edcodehub/edcodehub/EdCodehubApplication.java
@@ -0,0 +1,13 @@
+package com.edcodehub.edcodehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdCodehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdCodehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edcodehub/edcodehub/EdCodehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edcodehub.edcodehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdCodehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
